6bc3235af58f1cd46962abf459b48569d99e3dd1
Everton Vanoni Fernandes <86416772+vertocode@users.noreply.github.com>
2021-08-09 22:01:27 -0300

    Add files via upload

---
+++ voting machine/phases.js
@@ -0,0 +1,68 @@
+let phases = 
+[
+    /* {
+        title:'VEREADOR',
+        numbers: 5,
+        candidates: 
+        [ 
+            {
+                 number: '38111',
+            name: 'FULANO DE TAL',
+            party: 'ABC',
+            photos: 
+            [ 
+                {url:'vereador.png', subtitle: 'Vereador', small: false}
+            ] 
+            },
+            {
+                number: '77222',
+                name: 'Beltrano da Silva',
+                party: 'DEFG',
+                photos:
+                [
+                    {url:'president.jpg', subtitle: 'Vereador', small: false}
+                ]
+            }
+        ],
+
+    }, */
+    {
+        title: 'PRESIDENT',
+        numbers: 2,
+        candidates: 
+        [
+            {
+                number: '55',
+                name: 'Joe Biden',
+                party: 'Democratic Party',
+                vice: 'Kamala Harris',
+                photos:
+                [
+                    {url:'biden.jpeg', subtitle: 'President', small:true},
+                    {url:'kamala.jpg', subtitle: 'Vice-President', small:false}
+                ]
+            },
+            {
+                number: '67',
+                name: 'Donald Trump',
+                party: "Republican party",
+                vice:'Mike Pence',
+                photos:[
+                {url:'donald.jpg', subtitle: 'President', small:true},
+                {url:'mike.jpg', subtitle:'Vice-President', small:false}
+                ]
+            },
+            {
+                number: '40',
+                name: 'Hillary Clinton',
+                party: "Democratic Party",
+                vice:'Huma Abedin',
+                photos:[
+                {url:'hillary.jpg', subtitle: 'President', small:true},
+                {url:'Huma.jpg', subtitle:'Vice-President', small:false}
+                ]
+            }
+        ]
+    }
+
+]
